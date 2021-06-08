2b69d3e087f49a6ba531bdbff5c7a7d79b3192b4
jaojpaulo <jaojpaulosantos@gmail.com>
2021-06-07 23:08:12 -0300

    Configurar rotas Adicionar rotas no arquivo de inicialização do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import expres, { Request, Response } from 'express';
 
+import routes from './routes';
+
 const app = expres();
 
 app.use(expres.json());
-
-app.get('/', (request: Request, response: Response) => {
-  response.json({
-    mensagem: 'Funciona 🎈',
-  });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
