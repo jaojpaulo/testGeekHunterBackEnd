36f1e42d1a1f137903394797ead09b5b7b5ac335
jaojpaulo <jaojpaulosantos@gmail.com>
2021-06-24 22:11:21 -0300

    Remover importação não utlizada -Removendo importação não utilizada da interface do repositório do pivô de candidato e técnologia

--- src/modules/candidates/services/CreateCandidateService.ts
+++ src/modules/candidates/services/CreateCandidateService.ts
@@ -1,13 +1,21 @@
+/* eslint-disable no-await-in-loop */
+import { uuid } from 'uuidv4';
 import { inject, injectable } from 'tsyringe';
 
 import ICandidatesRepository from '../repositories/ICandidatesRepository';
+import ICandidateTechnologyRepository from '../repositories/ICandidateTechnologyRepository';
 
 import Candidate from '../infra/typeorm/entities/Candidate';
+
+import CandidateTechnology from '../infra/typeorm/entities/CandidateTechonlogy';
 
 interface IRequest {
   city: string;
   experience_id: string;
-  technologies: string[];
+  technologies: {
+    technology_id: string;
+    is_main_tech: boolean;
+  }[];
 }
 
 @injectable()
@@ -15,6 +23,9 @@ class CreateCandidateService {
   constructor(
     @inject('CandidatesRepository')
     private candidatesRepository: ICandidatesRepository,
+
+    @inject('CandidateTechnologyRepository')
+    private candidateTechnologyRepository: ICandidateTechnologyRepository,
   ) {}
 
   public async execute({
@@ -22,10 +33,25 @@ class CreateCandidateService {
     experience_id,
     technologies,
   }: IRequest): Promise<Candidate> {
+    let i: number;
+    const candidate_id = uuid();
+    const technology_list: CandidateTechnology[] = [];
+
+    for (i = 0; i < technologies.length; i += 1) {
+      technology_list.push(
+        await this.candidateTechnologyRepository.create({
+          candidate_id,
+          technology_id: technologies[i].technology_id,
+          is_main_tech: technologies[i].is_main_tech,
+        }),
+      );
+    }
+
     const candidate = await this.candidatesRepository.create({
+      id: candidate_id,
       city,
       experience_id,
-      technologies,
+      technology_list,
     });
 
     return candidate;
